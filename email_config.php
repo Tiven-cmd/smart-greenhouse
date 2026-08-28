e0737774d21760caee5fc8797032320a222b1476
Tiven <Tiven.shankar@gmail.com>
2026-08-28 14:36:58 +0800

    Update email configuration

--- email_config.php
+++ email_config.php
@@ -3,11 +3,9 @@
 return [
     'smtp_host' => 'smtp.gmail.com',
     'smtp_port' => 587,
-    'smtp_username' => 'smartgreenhouse07@gmail.com',
+    'smtp_username' => '_________',
 
-    // Put your NEW Google App Password here.
-    // Remove spaces when you paste it.
-    'smtp_password' => 'psvfufvivnefadtr',
+    'smtp_password' => '_________',
 
     'from_email' => 'smartgreenhouse07@gmail.com',
     'from_name' => 'Smart Greenhouse',
